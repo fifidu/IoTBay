2be2551aa3f641bebdf7253362aec0104076e555
Christopher Vuong <98904113+vuongchris@users.noreply.github.com>
2022-04-30 14:03:38 +1000

    Updated OrderBean.java, CartBean.java and CartLineBean.java

---
+++ IoTBay/src/java/uts/isd/model/CartLineBean.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package uts.isd.model;
+
+/**
+ *
+ * @author chrisvuong
+ */
+public class CartLineBean implements java.io.Serializable {
+
+    private int cartID;
+    private int productID;
+    private int quantity;
+
+    public CartLineBean() {
+
+    }
+
+    public int getCartID() {
+        return this.cartID;
+    }
+
+    public int getProductID() {
+        return this.productID;
+    }
+
+    public int getQuantity() {
+        return this.quantity;
+    }
+
+    public void setCartID(int cartID) {
+        this.cartID = cartID;
+    }
+
+    public void setProductID(int productID) {
+        this.productID = productID;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+}
---
+++ IoTBay/src/java/uts/isd/model/OrderBean.java
@@ -0,0 +1,100 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package uts.isd.model;
+
+import java.time.LocalDateTime;
+/**
+ *
+ * @author chrisvuong
+ */
+public class OrderBean implements java.io.Serializable {
+
+    private int orderID;
+    private int customerID;
+    private int cartID;
+    private int paymentID;
+    private int shippingID;
+    private LocalDateTime orderDate;
+    private String paymentStatus;
+    private String fulfillmentStatus;
+    private double totalCost;
+
+    public OrderBean() {
+
+    }
+
+    public int getOrderID() {
+        return this.orderID;
+    }
+
+    public int getCustomerID() {
+        return this.customerID;
+    }
+
+    public int getCartID() {
+        return this.cartID;
+    }
+
+    public int getPaymentID() {
+        return this.paymentID;
+    }
+
+    public int getShippingID() {
+        return this.shippingID;
+    }
+
+    public LocalDateTime getOrderDate() {
+        return this.orderDate;
+    }
+
+    public String getPaymentStatus() {
+        return this.paymentStatus;
+    }
+
+    public String getFulfillmentStatus() {
+        return this.fulfillmentStatus;
+    }
+
+    public double getTotalCost() {
+        return this.totalCost;
+    }
+
+    public void setOrderID(int orderID) {
+        this.orderID = orderID;
+    }
+
+    public void setCustomerID(int customerID) {
+        this.customerID = customerID;
+    }
+
+    public void setCartID(int cartID) {
+        this.cartID = cartID;
+    }
+
+    public void setPaymentID(int paymentID) {
+        this.paymentID = paymentID;
+    }
+
+    public void setShippingID(int shippingID) {
+        this.shippingID = shippingID;
+    }
+
+    public void setOrderDate(LocalDateTime orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public void setPaymentStatus(String paymentStatus) {
+        this.paymentStatus = paymentStatus;
+    }
+
+    public void setFulfillmentStatus(String fulfillmentStatus) {
+        this.fulfillmentStatus = fulfillmentStatus
+    }
+
+    public void setTotalCost(double totalCost) {
+        this.totalCost = totalCost;
+    }
+
+}
