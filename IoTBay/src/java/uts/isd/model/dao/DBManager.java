edfdc1763ef4467bcc8c4e4ac005092059fee57d
Christopher Vuong <98904113+vuongchris@users.noreply.github.com>
2022-05-01 22:15:37 +1000

    Updated OrderBean.java and DBManager.java
    OrderBean.java: Removed paymentID, shippingID, paymentStatus and fulfillmentStatus
    DBManager.java: Added empty methods for functions in the View Order Page

--- IoTBay/src/java/uts/isd/model/dao/DBManager.java
+++ IoTBay/src/java/uts/isd/model/dao/DBManager.java
@@ -10,5 +10,73 @@ import java.sql.Connection;
  * @author chrisvuong
  */
 public class DBManager {
-    
+
+    private statement st;
+
+    public DBManager(Connection conn) throws SQLException {
+        st = conn.createStatement();
+    }
+
+    /* Order Class */
+    /* Find Customer Orders */
+    public void findOrder(int customerID) throws SQLException {
+
+    }
+
+    /* Create Order */
+    public void addOrder(int customerID, int cartID) throws SQLException {
+
+    }
+
+    /* Update Order */
+    public void addOrderItem(int cartID, int productID, int quantity) throws SQLException {
+
+    }
+
+    public void updateOrderItem(int cartID, int productID, int quantity) throws SQLException {
+
+    }
+
+    public void deleteOrderItem(int cartID, int productID) throws SQLException {
+
+    }
+
+    public void updateOrderStatus(int orderID, String newStatus) throws SQLException {
+      
+    }
+
+    /* Delete Order */
+    public void deleteOrder(int cartID) throws SQLException {
+
+    }
+
+    /* Search Orders */
+    public void searchOrder(int customerID, String searchInput) throws SQLException {
+
+    }
+
+    /* Sort Orders */
+    public void sortByOrderIDAscend(int customerID) throws SQLException {
+
+    }
+
+    public void sortByOrderIDDescend(int customerID) throws SQLException {
+
+    }
+
+    public void sortByDateAscend(int customerID) throws SQLException {
+
+    }
+
+    public void sortByDateDescend(int customeraID) throws SQLException {
+
+    }
+
+    public void sortByTotalCostAscend(int customerID) throws SQLException {
+
+    }
+
+    public void sortByTotalCostDescend(int customerID) throws SQLException {
+
+    }
 }
