cf614815122464e22c01ae740784539d5dfa2412
Christopher Vuong <98904113+vuongchris@users.noreply.github.com>
2022-05-06 19:03:57 +1000

    Update shipping.sql

--- IoTBay/db/shipping.sql
+++ IoTBay/db/shipping.sql
@@ -22,10 +22,9 @@ CREATE TABLE shipping (
     CONSTRAINT shipping_FK FOREIGN KEY (orderID) REFERENCES orders
 );
 
-INSERT INTO shipping VALUES (1, '20 Australia Street', 'Newtown', 'NSW', 'Australia', '2042', 'Delivered', 'Newtown, Australia', '2020-07-01');
-INSERT INTO shipping VALUES (2, '206/101 Forest Road', 'Hurstville', 'NSW', 'Australia', '2220', 'Delivered', 'Hurstville, Australia', '2020-09-20');
-INSERT INTO shipping VALUES (3, '31 Watkins Road', 'Newcastle', 'NSW', 'Australia', '2267', 'Delivered', 'Newcastle, Australia', '2021-09-25')
-INSERT INTO shipping VALUES (4, '27 Mill Street', 'Carlton', 'VIC', 'Australia', '3053', 'In Transit', 'Melbourne, Australia', '2021-12-19');
-INSERT INTO shipping VALUES (5, '123 Sesame Street', 'Lazy Town','TX', 'United States of America', '99304', 'Delivered', 'Lazy Town, United States of America',  '2022-03-14');
-
+INSERT INTO shipping (orderID, addressStreet, addressCity, addressState, addressCountry, addressPostal, orderStatus, currentLocation, estDelivery) VALUES (1, '20 Australia Street', 'Newtown', 'NSW', 'Australia', '2042', 'Delivered', 'Newtown, Australia', '2020-07-01');
+INSERT INTO shipping (orderID, addressStreet, addressCity, addressState, addressCountry, addressPostal, orderStatus, currentLocation, estDelivery) VALUES (2, '206/101 Forest Road', 'Hurstville', 'NSW', 'Australia', '2220', 'Delivered', 'Hurstville, Australia', '2020-09-20');
+INSERT INTO shipping (orderID, addressStreet, addressCity, addressState, addressCountry, addressPostal, orderStatus, currentLocation, estDelivery) VALUES (3, '31 Watkins Road', 'Newcastle', 'NSW', 'Australia', '2267', 'Delivered', 'Newcastle, Australia', '2021-09-25')
+INSERT INTO shipping (orderID, addressStreet, addressCity, addressState, addressCountry, addressPostal, orderStatus, currentLocation, estDelivery) VALUES (4, '27 Mill Street', 'Carlton', 'VIC', 'Australia', '3053', 'In Transit', 'Melbourne, Australia', '2021-12-19');
+INSERT INTO shipping (orderID, addressStreet, addressCity, addressState, addressCountry, addressPostal, orderStatus, currentLocation, estDelivery) VALUES (5, '123 Sesame Street', 'Lazy Town','TX', 'United States of America', '99304', 'Delivered', 'Lazy Town, United States of America', '2022-03-14');
 SELECT * FROM shipping;
